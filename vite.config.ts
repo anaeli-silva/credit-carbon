a89587ab983d7b6daf40f1531105f5981fd408d5
Skinzin <SkinzinSTG_@hotmail.com>
2024-11-05 06:40:23 -0300

    Config env for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
-import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(), 
-    svgr({
-      include: '**/*.svg?react',
-    }),
-    vercel()
-  ],
-  resolve: {
-    alias: {
-      '@': '/src'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react(), 
+      svgr({
+        include: '**/*.svg?react',
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': '/src'
+      },
     },
-  },
+    define: {
+      REACT_APP_OLLAMA_URL: env.VITE_APP_OLLAMA_URL,
+    }
+  }
 })
