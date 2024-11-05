80a7ceea79a9ff416bb5fbba44174548ae12a6d2
Skinzin <SkinzinSTG_@hotmail.com>
2024-11-05 17:21:07 -0300

    config axios

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,9 +2,18 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: "https://b4a4-2804-7f0-14-2c35-816e-28d5-4ee2-93b4.ngrok-free.app",
+  headers: {
+    "Content-Type": "application/json",
+    common: {
+      "Access-Control-Allow-Origin": "*",
+    }
+  },
 });
 
+// // Configure CORS
+// api.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
 export {
-    api
+  api
 }
 
