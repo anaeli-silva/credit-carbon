476ecdcf1c503823ee9056d7e9b49bda19c2390a
Skinzin <SkinzinSTG_@hotmail.com>
2024-11-09 00:57:42 -0300

    ENV key

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://b4a4-2804-7f0-14-2c35-816e-28d5-4ee2-93b4.ngrok-free.app",
+  baseURL: import.meta.env.VITE_APP_OLLAMA_URL,
 });
 
 export {
